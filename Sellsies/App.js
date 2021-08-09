af382218604c1632a67b0d4841ad03714389e887
Bea <bea.jayme14@gmail.com>
2021-08-09 22:04:23 +1200

    Add onPress functionality to Text component

--- Sellsies/App.js
+++ Sellsies/App.js
@@ -3,9 +3,13 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
+  const handlePress = () => console.log("Text pressed!");
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text numberOfLines={1} onPress={handlePress}>
+        Hello world!
+      </Text>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
