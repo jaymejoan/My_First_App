76221d5a28e54bf086a255dea53c7ddd410241ed
Bea <bea.jayme14@gmail.com>
2021-08-09 22:50:25 +1200

    Add Dimensions componenet and use percentage values instead of pixels

--- Sellsies/App.js
+++ Sellsies/App.js
@@ -2,35 +2,34 @@
 import React from "react";
 import {
   StyleSheet,
-  Text,
-  View,
-  Image,
   SafeAreaView,
   StatusBar,
-  Button,
   Platform,
+  Dimensions,
+  View,
 } from "react-native";
 
 export default function App() {
+  console.log(Dimensions.get("screen"));
+
   return (
     <SafeAreaView style={styles.container}>
-      <Button
-        color="orange"
-        title="Click me"
-        onPress={() =>
-          Alert.prompt("My title", "My message", (text) => console.log(text))
-        }
-      />
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: "30%",
+        }}
+      ></View>
     </SafeAreaView>
   );
 }
-
-const containerStyle = { backgroundColor: "orange" };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
