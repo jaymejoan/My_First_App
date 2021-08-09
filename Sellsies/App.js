6c36f4c0ede55aa92fdc16094ecbce361660388e
Bea <bea.jayme14@gmail.com>
2021-08-09 22:10:21 +1200

    Render local and web images

--- Sellsies/App.js
+++ Sellsies/App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
-  const handlePress = () => console.log("Text pressed!");
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
-        Hello world!
-      </Text>
+      <Text> Hello World! </Text>
+      {/* <Image source={require("./assets/icon.png")} /> */}
+      <Image
+        source={{
+          width: 200,
+          height: 300,
+          uri: "https://picsum.photos/200/300",
+        }}
+      />
       <StatusBar style="auto" />
     </SafeAreaView>
   );
