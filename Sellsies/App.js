bb07b38295df411e7d5454258a4d9a96eb8856df
Bea <bea.jayme14@gmail.com>
2021-08-09 22:23:29 +1200

    Add Button component

--- Sellsies/App.js
+++ Sellsies/App.js
@@ -3,30 +3,20 @@ import React from "react";
 import {
   StyleSheet,
   Text,
-  TouchableWithoutFeedback,
-  TouchableOpacity,
-  TouchableHighlight,
   View,
   Image,
   SafeAreaView,
+  Button,
 } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text> Hello World! </Text>
-      {/* <Image source={require("./assets/icon.png")} /> */}
-
-      <TouchableOpacity onPress={() => console.log("Image tapped.")}>
-        <Image
-          source={{
-            width: 200,
-            height: 300,
-            uri: "https://picsum.photos/200/300",
-          }}
-        />
-      </TouchableOpacity>
-      <StatusBar style="auto" />
+      <Button
+        color="orange"
+        title="Click me"
+        onPress={() => console.log("Button tapped.")}
+      />
     </SafeAreaView>
   );
 }
