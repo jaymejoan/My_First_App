690027914cea0c72489a905b78c3ed3447da3de1
Bea <bea.jayme14@gmail.com>
2021-08-09 22:20:15 +1200

    Add Touchable features to Image component

--- Sellsies/App.js
+++ Sellsies/App.js
@@ -1,19 +1,31 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  View,
+  Image,
+  SafeAreaView,
+} from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <Text> Hello World! </Text>
       {/* <Image source={require("./assets/icon.png")} /> */}
-      <Image
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
+
+      <TouchableOpacity onPress={() => console.log("Image tapped.")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
