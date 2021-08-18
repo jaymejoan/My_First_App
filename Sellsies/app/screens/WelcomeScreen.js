1bd913bac417913410df58d507e0b14aa4bcac02
Bea <bea.jayme14@gmail.com>
2021-08-18 23:28:55 +1200

    Add logo to WelcomeScreen

--- Sellsies/app/screens/WelcomeScreen.js
+++ Sellsies/app/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ImageBackground, StyleSheet, View } from "react-native";
+import { Image, ImageBackground, StyleSheet, View } from "react-native";
 
 function WelcomeScreen(props) {
   return (
@@ -7,6 +7,7 @@ function WelcomeScreen(props) {
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
+      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
       <View style={styles.loginButton}></View>
       <View style={styles.registerButton}></View>
     </ImageBackground>
@@ -16,12 +17,19 @@ function WelcomeScreen(props) {
 const styles = StyleSheet.create({
   background: {
     flex: 1,
-    justifyContent: "flex-end"
+    justifyContent: "flex-end",
+    alignItems: "center",
   },
   loginButton: {
     width: "100%",
     height: 70,
     backgroundColor: "#fc5c65",
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    position: "absolute",
+    top: 80,
   },
   registerButton: {
     width: "100%",
