882fc34a52bbf157e1d1e930d584875d6038c661
Bea <bea.jayme14@gmail.com>
2021-08-19 00:08:15 +1200

    Add background image, close and delete button

--- Sellsies/app/screens/ViewImageScreen.js
+++ Sellsies/app/screens/ViewImageScreen.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React from "react";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
 
 function ViewImageScreen(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <ImageBackground
+      style={styles.background}
+      source={require("../assets/chair.jpg")}
+    >
+      <View style={styles.closeButton} />
+      <View style={styles.deleteButton} />
+    </ImageBackground>
+  );
 }
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        resizeMode: "contain",
+    },
+    closeButton: {
+        width: 100,
+        height: 100,
+        backgroundColor: "#fc5c65",
+    },
+    closeButton: {
+        width: 100,
+        height: 100,
+        backgroundColor: "#4ECDC4",
+    }
+});
 
 export default ViewImageScreen;
